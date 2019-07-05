33be7aac50793a6fb8afbe9fd817f9755365795b
Alexandra Kapp <alexandra.kapp2@gmail.com>
2019-07-05 14:56:53 +0200

    test and create code for complex queries

--- datenguide_python/query_builder.py
+++ datenguide_python/query_builder.py
@@ -1,23 +1,47 @@
-class Query:
+class QueryBuilder():
 
-    def __init__(self, region: str, *args, **kwargs):
+    def __init__(self, region: str, fields: list):
         # must have region
-        # at least one subfield
-        # args for subfields
-        # id name and statistics possible as args
+        # filter for region: id
 
-        # for statistics possible args: year, value, source, id, xxx
+        # at least one subfield: fields
+        # complex subfields
+        # query: id, simple fields (id, name), complex fields (statistics with args(id, year, value, source, xx))
+        # complex fields: (args, fields)
+
+        # get_fields()
 
         self.region = region
-        self.fields = kwargs
+        #self.nuts = 
+        self.fields = fields
         # self.__dict__.update(kwargs)
 
-    def get_graphql_query(self):
-        # TODO: return all fields
+    def _get_fields_to_query(self) -> str:
+        fields_string = ""
+        for field in self.fields:
+            if isinstance(field, str):
+                fields_string += field + " "
+            elif isinstance(field, ComplexField):
+                fields_string += field.statistic
+                for key, value in field.filters.items():
+                    fields_string += "(" + key + ": " + str(value) + ")"
+                fields_string += "{" + " ".join(field.fields) + "} "
+
+        print(fields_string)
+        return fields_string
+
+    def get_graphql_query(self) -> str:
         return """
                 {
-                    region(id:\"""" + self.region + """\") {
-                        """ + " ".join(list(self.fields.values())) + """
+                    region(id: \"""" + self.region + """\") {
+                        """ + self._get_fields_to_query() + """
                     }
                 }
-                """          
+                """    
+
+class ComplexField():
+
+    def __init__(self, statistic, fields: list, filters: dict = None):
+        self.statistic = statistic
+        self.fields = fields
+        self.filters = filters
--- tests/test_query_builder.py
+++ tests/test_query_builder.py
@@ -1,8 +1,8 @@
 
 import pytest
 
-import datenguide_python
-from datenguide_python import query_builder
+import datenguide_python as dg
+# from datenguide_python import query_builder
 
 
 @pytest.fixture
@@ -16,23 +16,72 @@ def args():
 
 
 def test_create_query_class_with_args_instance():
-    query = query_builder.Query(region='09', field1='BEV001')
-    assert isinstance(query, query_builder.Query)
+    query = dg.QueryBuilder(region='09', fields=['BEV001'])
+    assert isinstance(query, dg.QueryBuilder)
 
 
 def test_create_query_class_without_args_throws_error():
     with pytest.raises(TypeError):
-        query_builder.Query()
+        dg.QueryBuilder()
 
 
 def test_basic_graphql_string():
-    query = query_builder.Query(region='09', field1='BEV001')
-    print(query.get_graphql_query())
+    query = dg.QueryBuilder(region='09', fields=['BEV001'])
     graphql_query = query.get_graphql_query()
     assert graphql_query == """
                 {
-                    region(id:"09") {
-                        BEV001
+                    region(id: "09") {
+                        BEV001 
                     }
                 }
-                """     
+                """   
+
+
+def test_get_fields_to_query():
+    statistic = dg.ComplexField(
+            statistic='WAHL09',
+            filters={'year': 2017},
+            fields=['value', 'PART04'])
+    query = dg.QueryBuilder(region='09', fields=[statistic])
+    fields_string = query._get_fields_to_query()
+    assert fields_string == "WAHL09(year: 2017){value PART04} "
+
+
+def test_get_complex_graphql_string():
+        statistic = dg.ComplexField(
+            statistic='WAHL09',
+            filters={'year': 2017},
+            fields=['value', 'PART04'])
+        query = dg.QueryBuilder(region='09', fields=['id', 'name', statistic])
+        graphql_query = query.get_graphql_query()
+        assert graphql_query == """
+                {
+                    region(id: "09") {
+                        id name WAHL09(year: 2017){value PART04} 
+                    }
+                }
+                """
+
+
+def test_get_multiple_stats():
+        statistic1 = dg.ComplexField(
+            statistic='WAHL09',
+            filters={'year': 2017},
+            fields=['value', 'PART04'])
+
+        statistic2 = dg.ComplexField(
+            statistic='BEV001',
+            filters={'statistics': 'R12612'},
+            fields=['value', 'year'])
+
+        query = dg.QueryBuilder(
+            region='09',
+            fields=['id', 'name', statistic1, statistic2])
+        graphql_query = query.get_graphql_query()
+        assert graphql_query == """
+                {
+                    region(id: "09") {
+                        id name WAHL09(year: 2017){value PART04} BEV001(statistics: R12612){value year} 
+                    }
+                }
+                """
