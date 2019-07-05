275674ccc5f395be0b8b65c7b507347f51da7404
Alexandra Kapp <alexandra.kapp2@gmail.com>
2019-07-05 14:56:21 +0200

    add classes to init

--- datenguide_python/__init__.py
+++ datenguide_python/__init__.py
@@ -3,5 +3,8 @@
 """Top-level package for Datenguide Python."""
 
 __author__ = """Alexandra Kapp"""
-__email__ = 'xxx@correlaid.org'
-__version__ = '0.1.0'
+__email__ = "xxx@correlaid.org"
+__version__ = "0.1.0"
+
+from datenguide_python.query_builder import QueryBuilder
+from datenguide_python.query_builder import ComplexField
