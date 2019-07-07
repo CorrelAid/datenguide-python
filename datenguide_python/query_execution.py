d26c708a340603be7383645754fe4057ba85df95
KonradUdoHannes <konrad.woelms@gmail.com>
2019-07-07 23:19:10 +0200

    updated tests and set up dummy implementation

---
+++ datenguide_python/query_execution.py
@@ -0,0 +1,6 @@
+class QueryExecutioner(object):
+    def __init__(self, endpoint: str = "https://api-next.datengui.de/graphql") -> None:
+        self.endpoint = endpoint
+
+    def run_query(self):
+        pass
