79d1bdab78709e66df2ef4b6cc1ce5a948ddadb9
konradwoelms <konrad.woelms@catanacapital.de>
2019-07-27 07:58:29 +0200

    added metda data query functionlity for arguments and types

--- datenguide_python/query_execution.py
+++ datenguide_python/query_execution.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, cast, Optional, NamedTuple, List
+from typing import Dict, Any, cast, Optional, NamedTuple, List, Tuple
 import requests
 import re
 
@@ -11,11 +11,36 @@ class ExecutionResults(NamedTuple):
 
 
 class FieldMetaDict(dict):
-    def get_return_type(self):
+    def get_return_type(self) -> str:
         if self["type"]["kind"] == "LIST":
             return self["type"]["ofType"]["name"]
         else:
             return self["type"]["name"]
+
+    def get_arguments(self) -> List[Tuple[Optional[str], ...]]:
+        def get_type_of(
+            argument: Dict[str, Any]
+        ) -> Tuple[Optional[str], Optional[str]]:
+            if argument["type"]["ofType"]:
+                return (
+                    argument["type"]["ofType"]["name"],
+                    argument["type"]["ofType"]["kind"],
+                )
+            else:
+                return None, None
+
+        return list(
+            (
+                cast(Optional[str], arg.get("name")),
+                cast(Optional[str], arg.get("type", {}).get("kind", {})),
+                cast(Optional[str], arg.get("type", {}).get("name")),
+                *get_type_of(arg),
+            )
+            for arg in self["args"]
+        )
+
+    def get_enumValues(self) -> List[Json]:
+        return self["enumValues"]
 
 
 class QueryExecutioner(object):
@@ -24,25 +49,12 @@ class QueryExecutioner(object):
 
     _META_DATA_CACHE: Json = dict()
 
-    _meta_data_query_json: Dict[str, str] = {
-        "query": r"""{
-          __type(name: "Region") {
-            fields {
-              name
-              description
-              args {
-                name
-              }
-            }
-          }
-        }"""
-    }
-
-    _meta_type_info = """
+    _meta_type_info: str = """
         query TypeInfo($type: String!) {
       __type(name: $type) {
         fields {
           name
+          enumValues
           type {
             ofType {
               name
@@ -93,34 +105,36 @@ class QueryExecutioner(object):
         else:
             return None
 
-    def _get_type_info(self, type):
-        if type in self.__class__._META_DATA_CACHE:
-            return self.__class__._META_DATA_CACHE[type]
-        variables = {"type": type}
-        query_json = {}
+    def get_type_info(self, graph_ql_type: str) -> Optional[Json]:
+        if graph_ql_type in self.__class__._META_DATA_CACHE:
+            print("use cache")
+            return self.__class__._META_DATA_CACHE[graph_ql_type]
+        variables: Dict[str, str] = {"type": graph_ql_type}
+        query_json: Json = {}
         query_json["query"] = self._meta_type_info
         query_json["variables"] = variables
+        print("query database")
         info = self._send_request(query_json)
         if info:
             type_meta = {
                 f["name"]: FieldMetaDict(f) for f in info["data"]["__type"]["fields"]
             }
-            self.__class__._META_DATA_CACHE[type] = type_meta
+            self.__class__._META_DATA_CACHE[graph_ql_type] = type_meta
             return type_meta
         else:
-            None
+            return None
 
     @staticmethod
     def _generate_post_json(
         query, variables=Optional[Dict[str, str]]
     ) -> Dict[str, str]:
-        post_json = dict()
+        post_json: Json = dict()
         post_json["query"] = query.get_graphql_query()
         if variables:
             post_json["variables"] = cast(Dict[str, str], variables)
         return post_json
 
-    def _send_request(self, query_json: Dict[str, str]) -> Optional[Json]:
+    def _send_request(self, query_json: Json) -> Optional[Json]:
         resp = requests.post(
             self.endpoint, headers=self.REQUEST_HEADER, json=query_json
         )
@@ -131,7 +145,7 @@ class QueryExecutioner(object):
             return None
 
     def _get_stats_meta_data(self) -> Optional[Json]:
-        return self._send_request(self._meta_data_query_json)
+        return self.get_type_info("Region")
 
     @staticmethod
     def _process_stat_meta_data(raw_response: Json) -> List[Json]:
