87e975e50d81c7b035c5caeba0c8f867a7d21907
bhatgithub <barbara.hofmann@gmail.com>
2019-08-13 00:17:34 +0200

    update setup.py and output_transformer.py

--- datenguide_python/output_transformer.py
+++ datenguide_python/output_transformer.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from pandas.io.json import json_normalize
 
+from typing import List, Dict, Any
+
 
 class QueryOutputTransformer:
     """ IN PROGRESS only region query to do: DOKU """
@@ -12,84 +14,59 @@ class QueryOutputTransformer:
         pass
 
     @staticmethod
-    def flatten_query_result(dic):
-        flat = json_normalize(dic)
-        return flat
-
-    @staticmethod
     def get_col_list(df):
-        c = list(df.columns)
-        try:
-            c.remove("data.region.id")
-        except ValueError:
-            pass
-        try:
-            c.remove("data.region.name")
-        except ValueError:
-            pass
+        c = [
+            col
+            for col in df.columns
+            if col not in ["data.region.id", "data.region.name"]
+        ]
         return c
 
     @staticmethod
-    def build_and_merge_data(dic, lis, year_default=True):
-        data = json_normalize(dic[lis[0]][0])
-        if year_default is True:
-            data.columns = [lis[0], "year"]
-            for d in lis[1:]:
-                if json_normalize(dic[d][0]).empty is True:
+    def build_and_merge_data(
+        query_response_json: Dict[str, Any],
+        some_fields: List[str],
+        year_default: bool = True,
+    ) -> pd.DataFrame:
+        data = json_normalize(query_response_json[some_fields[0]][0])
+        if year_default:
+            data.columns = [some_fields[0], "year"]
+            for d in some_fields[1:]:
+                if json_normalize(query_response_json[d][0]).empty:
                     temp = pd.DataFrame([np.nan, np.nan]).T
                     temp.columns = [d, "year"]
-                elif json_normalize(dic[d][0]).empty is False:
-                    temp = json_normalize(dic[d][0])
+                elif not json_normalize(query_response_json[d][0]).empty:
+                    temp = json_normalize(query_response_json[d][0])
                     try:
                         temp.columns = [d, "year"]
                         print("no type variable")
                     except ValueError:
-                        newcolname = temp.values[0][0] + "_" + d
+                        new_column_name = temp.iloc[0, 0] + "_" + d
                         temp = temp.iloc[:, 1:]
-                        temp.columns = [newcolname, "year"]
+                        temp.columns = [new_column_name, "year"]
                         print("type variable")
-                data = data.merge(temp, on="year", how="outer")
+                data = data.merge(temp, how="outer")
 
             newcol = [x.replace("data.region.", "") for x in data.columns]
             data.columns = newcol
             try:
-                data["id"] = dic["data.region.id"][0]
-            except ValueError:
+                data["id"] = query_response_json["data.region.id"][0]
+            except KeyError:
                 pass
             try:
-                data["name"] = dic["data.region.name"][0]
-            except ValueError:
+                data["name"] = query_response_json["data.region.name"][0]
+            except KeyError:
                 pass
 
-        if year_default is False:
-            data.columns = [lis[0]]
-            for d in lis[1:]:
-                temp = json_normalize(dic[d][0])
-                try:
-                    temp.columns = [d]
-                    print("no type variable")
-                except ValueError:
-                    newcolname = data.values[0][0] + "_" + d
-                    temp = temp.iloc[:, 1:]
-                    temp.columns = [newcolname]
-                    print("type variable")
-                pd.concat([data, temp], axis=1)
+        if not year_default:
+            print(' "year" missing. query must contain "year"!')
+            data = pd.DataFrame([])
 
-            newcol = [x.replace("data.region.", "") for x in data.columns]
-            data.columns = newcol
-            try:
-                data["id"] = dic["data.region.id"][0]
-            except ValueError:
-                pass
-            try:
-                data["name"] = dic["data.region.name"][0]
-            except ValueError:
-                pass
         return data
 
     @classmethod
     def transform(cls, dic, year_default=True):
-        flat = cls.flatten_query_result(dic)
+        flat = json_normalize(dic)
         c = cls.get_col_list(flat)
         output = cls.build_and_merge_data(flat, c, year_default)
         return output
