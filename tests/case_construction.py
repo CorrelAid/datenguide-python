2f6c0f16082ce05072654554f58a58e34838f7b7
konradudohannes <konrad.woelms@gmail.com>
2019-12-22 17:36:56 +0100

    implemented case_construction functionality to serialize query execution results into files in order to generate internet/API indipendent test cases

--- tests/case_construction.py
+++ tests/case_construction.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from datenguidepy.query_execution import ExecutionResults
+from datenguidepy.query_execution import ExecutionResults, QueryExecutioner
 
 
 def construct_execution_results(path):
@@ -20,4 +20,18 @@ def save_result_files_from_query(query, name, result_path):
         Helper function that for the construction of files
         to be loaded by `construct_execution_results`
     """
-    raise NotImplementedError
+    qe = QueryExecutioner()
+    execution_results = qe.run_query(query)
+
+    query_json_file = os.path.join(result_path, f"{name}_query.json")
+    result_json_file = os.path.join(result_path, f"{name}.json")
+    meta_json_file = os.path.join(result_path, f"{name}_meta.json")
+
+    with open(query_json_file, "w") as file:
+        json.dump(query.get_graphql_query(), file)
+
+    with open(result_json_file, "w") as file:
+        json.dump([res.query_results for res in execution_results], file)
+
+    with open(meta_json_file, "w") as file:
+        json.dump(execution_results[0].meta_data, file)
