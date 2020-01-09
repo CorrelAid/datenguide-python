d89ec2a65e4b5ce4737218dd323ff573217d2682
Alexandra Kapp <alexandra.kapp2@gmail.com>
2020-01-10 00:07:08 +0100

    mock graphql meta data provider

--- tests/functional_tests/test_query_builder.py
+++ tests/functional_tests/test_query_builder.py
@@ -4,95 +4,155 @@ import sys
 import io
 import pandas as pd
 from datenguidepy import Field, Query
-from datenguidepy.query_execution import FieldMetaDict, TypeMetaData
+from datenguidepy.query_execution import (
+    FieldMetaDict,
+    TypeMetaData,
+    GraphQlSchemaMetaDataProvider,
+)
 
 
 @pytest.fixture
-def mocked_meta_data(monkeypatch):
-    def mock_enum_placeholder(self):
-        return "MOCKED ENUM VALUES"
-
-    def mock_get_arguments(self):
-        mocked_args = {
-            "year": ("LIST", None, "SCALAR", "Int"),
-            "statistics": ("LIST", None, "ENUM", "BEV001Statistics"),
-            "ALTMT1": ("LIST", None, "ENUM", "ALTMT1"),
-        }
-        return mocked_args
-
-    def mock_get_description(self, *args):
-        return "Lebend Geborene"
-
-    def mock_field_meta_data(self):
-
-        type_kind = "kind='OBJECT'"
-        field_meta = {
-            "id": FieldMetaDict(
-                {
-                    "name": "id",
-                    "type": {
-                        "ofType": None,
-                        "kind": "SCALAR",
-                        "name": "String",
-                        "description": "The `String` scalar...",
-                    },
-                    "description": "Interne eindeutige ID",
-                    "args": [],
-                }
-            ),
-            "year": FieldMetaDict(
-                {
-                    "name": "year",
-                    "type": {
-                        "ofType": None,
-                        "kind": "SCALAR",
-                        "name": "Int",
-                        "description": "The `Int` scalar...",
-                    },
-                    "description": "Jahr des Stichtages",
-                    "args": [],
-                }
-            ),
-            "value": FieldMetaDict(
-                {
-                    "name": "value",
-                    "type": {
-                        "ofType": None,
-                        "kind": "SCALAR",
-                        "name": "Float",
-                        "description": "The `Float` scalar...",
-                    },
-                    "description": "Wert",
-                    "args": [],
-                }
-            ),
-        }
+def mock_graphqlschemaprovider(monkeypatch):
+    def mock_meta_data(self, return_type):
+        type_kind = "OBJECT"
         enum_vals = None
-        return Field._no_none_values(
-            self._fields_info_formatter,
-            TypeMetaData(type_kind, field_meta, enum_vals),
-            "fields",
-        )
+        if return_type == "Region":
+            field_meta = {
+                "id": FieldMetaDict(
+                    {
+                        "name": "id",
+                        "type": {
+                            "ofType": None,
+                            "kind": "SCALAR",
+                            "name": "String",
+                            "description": "The `String` scalar type ...",
+                        },
+                        "description": "Regionalschlüssel",
+                        "args": [],
+                    }
+                ),
+                "name": FieldMetaDict(
+                    {
+                        "args": [],
+                        "description": "Name",
+                        "name": "name",
+                        "type": {
+                            "description": "The `String` scalar...",
+                            "kind": "SCALAR",
+                            "name": "String",
+                            "ofType": None,
+                        },
+                    }
+                ),
+                "BEV001": FieldMetaDict(
+                    {
+                        "name": "BEV001",
+                        "type": {
+                            "ofType": {"name": "BEV001"},
+                            "kind": "LIST",
+                            "name": None,
+                            "description": None,
+                        },
+                        "description": "Statistik der Geburten",
+                        "args": [
+                            {
+                                "name": "year",
+                                "type": {
+                                    "kind": "LIST",
+                                    "name": None,
+                                    "ofType": {
+                                        "name": "Int",
+                                        "description": "The `Int` scalar type ...",
+                                        "kind": "SCALAR",
+                                    },
+                                },
+                            },
+                            {
+                                "name": "NAT",
+                                "type": {
+                                    "kind": "LIST",
+                                    "name": None,
+                                    "ofType": {
+                                        "name": "NAT",
+                                        "description": "Nationalität",
+                                        "kind": "ENUM",
+                                    },
+                                },
+                            },
+                        ],
+                    }
+                ),
+            }
+        elif return_type == "BEV001":
+            field_meta = {
+                "id": FieldMetaDict(
+                    {
+                        "name": "id",
+                        "type": {
+                            "ofType": None,
+                            "kind": "SCALAR",
+                            "name": "String",
+                            "description": "The `String` scalar...",
+                        },
+                        "description": "Interne eindeutige ID",
+                        "args": [],
+                    }
+                ),
+                "year": FieldMetaDict(
+                    {
+                        "args": [...],
+                        "description": "Jahr des Stichtages",
+                        "name": "year",
+                        "type": {
+                            "description": "The `Int` scalar...",
+                            "kind": "SCALAR",
+                            "name": "Int",
+                            "ofType": None,
+                        },
+                    }
+                ),
+                "value": FieldMetaDict(
+                    {
+                        "args": [...],
+                        "description": "Wert",
+                        "name": "value",
+                        "type": {
+                            "description": "The `Float` scalar ...",
+                            "kind": "SCALAR",
+                            "name": "Float",
+                            "ofType": None,
+                        },
+                    }
+                ),
+                "NAT": FieldMetaDict(
+                    {
+                        "args": [...],
+                        "description": "Nationalität",
+                        "name": "NAT",
+                        "type": {
+                            "description": "",
+                            "kind": "ENUM",
+                            "name": "NAT",
+                            "ofType": None,
+                        },
+                    }
+                ),
+            }
+        elif return_type == "NAT":
+            type_kind = "ENUM"
+            field_meta = None
+            enum_vals = {
+                "NAA": "Ausländer(innen)",
+                "NAD": "Deutsche",
+                "GESAMT": "Gesamt",
+            }
 
-    monkeypatch.setattr(Field, "_get_description", mock_get_description)
-    monkeypatch.setattr(FieldMetaDict, "get_arguments", mock_get_arguments)
-    monkeypatch.setattr(Field, "enum_info", mock_enum_placeholder)
-    monkeypatch.setattr(Field, "fields_info", mock_field_meta_data)
+        else:
+            raise RuntimeError("return type not in Monkeypatch")
 
+        return TypeMetaData(type_kind, field_meta, enum_vals)
 
-@pytest.fixture
-def mocked_enum_meta_data(monkeypatch):
-    def mock_enum_meta_data(self):
-        type_kind = "kind='ENUM'"
-        field_meta = None
-        enum_vals = {"NAA": "Ausländer(innen)", "NAD": "Deutsche", "GESAMT": "Gesamt"}
-        return Field._no_none_values(
-            self._enum_info_formatter,
-            TypeMetaData(type_kind, field_meta, enum_vals),
-            "enum_values",
-        )
-
-    monkeypatch.setattr(Field, "enum_info", mock_enum_meta_data)
+    monkeypatch.setattr(GraphQlSchemaMetaDataProvider, "get_type_info", mock_meta_data)
 
 
 @pytest.fixture
@@ -172,41 +232,40 @@ def test_process_query(query_default):
     assert isinstance(df, pd.DataFrame)
 
 
-def test_arguments_info_mocked(mocked_meta_data, query_default):
-    stat = query_default.add_field("BEV001")
+def test_arguments_info_mocked(mock_graphqlschemaprovider, query):
+    stat = query.add_field("BEV001")
     info = stat.arguments_info()
     expected_info = """\x1b[4myear\x1b[0m: LIST of type SCALAR(Int)
 
-    \x1b[4mstatistics\x1b[0m: LIST of type ENUM(BEV001Statistics)
+    \x1b[4mNAT\x1b[0m: LIST of type ENUM(NAT)
     enum values:
-    MOCKED ENUM VALUES
-
-    \x1b[4mALTMT1\x1b[0m: LIST of type ENUM(ALTMT1)
-    enum values:
-    MOCKED ENUM VALUES"""
+    NAA: Ausländer(innen)
+    NAD: Deutsche
+    GESAMT: Gesamt"""
 
     assert re.sub(r"\s+", "", info) == re.sub(r"\s+", "", expected_info)
 
 
-def test_field_info_mocked(mocked_meta_data, query_default):
-    stat = query_default.add_field("BEV001")
+def test_field_info_mocked(mock_graphqlschemaprovider, query):
+    stat = query.add_field("BEV001")
     info = stat.fields_info()
     expected_info = """
     id: Interne eindeutige ID
     year: Jahr des Stichtages
     value: Wert
+    NAT: Nationalität
     """
     assert re.sub(r"\s+", "", info) == re.sub(r"\s+", "", expected_info)
 
 
-def test_description_mocked(mocked_meta_data, query_default):
-    stat = query_default.add_field("BEV001")
+def test_description_mocked(mock_graphqlschemaprovider, query):
+    stat = query.add_field("BEV001")
     descr = stat.description()
-    assert descr == "Lebend Geborene"
+    assert descr == "Statistik der Geburten"
 
 
-def test_enum_mocked(mocked_enum_meta_data, query_default):
-    stat = query_default.add_field("BEV001").add_field("NAT")
+def test_enum_mocked(mock_graphqlschemaprovider, query):
+    stat = query.add_field("BEV001").add_field("NAT")
     info = stat.enum_info()
     expected_info = """
     NAA: Ausländer(innen)
@@ -216,36 +275,35 @@ def test_enum_mocked(mocked_enum_meta_data, query_default):
     assert re.sub(r"\s+", "", info) == re.sub(r"\s+", "", expected_info)
 
 
-def test_get_info_stat_mocked(mocked_meta_data, query_default):
+def test_get_info_stat_mocked(mock_graphqlschemaprovider, query):
     stringio = io.StringIO()
     sys.stdout = stringio
-    stat = query_default.add_field("BEV001")
+    stat = query.add_field("BEV001")
     stat.get_info()
     info = stringio.getvalue()
     expected_info = """\x1b[1mkind:\x1b[0m
         OBJECT
 
         \x1b[1mdescription:\x1b[0m
-        Lebend Geborene
+        Statistik der Geburten
 
         \x1b[1marguments:\x1b[0m
         \x1b[4myear\x1b[0m: LIST of type SCALAR(Int)
 
-        \x1b[4mstatistics\x1b[0m: LIST of type ENUM(BEV001Statistics)
+        \x1b[4mNAT\x1b[0m: LIST of type ENUM(NAT)
         enum values:
-        MOCKED ENUM VALUES
-
-        \x1b[4mALTMT1\x1b[0m: LIST of type ENUM(ALTMT1)
-        enum values:
-        MOCKED ENUM VALUES
+        NAA: Ausländer(innen)
+        NAD: Deutsche
+        GESAMT: Gesamt
 
         \x1b[1mfields:\x1b[0m
         id: Interne eindeutige ID
         year: Jahr des Stichtages
         value: Wert
+        NAT: Nationalität
 
         \x1b[1menum values:\x1b[0m
-        MOCKED ENUM VALUES"""
+        None"""
 
     assert re.sub(r"\s+", "", info) == re.sub(r"\s+", "", expected_info)
 
