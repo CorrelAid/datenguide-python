b49f8c6da83a6568a28aee300a26b2d769dd4f09
konradudohannes <konrad.woelms@gmail.com>
2020-05-17 13:07:06 +0200

    added history for 0.2 release, created a requirements_tox for tox and travis and chagned the packagge pandas requirement for pandas to >1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["pandas>=0.25.0", "requests", "typing_extensions"]
+requirements = ["pandas>=1.0.0", "requests", "typing_extensions"]
 
 setup_requirements = ["pytest-runner"]
 
