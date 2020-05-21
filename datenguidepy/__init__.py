2ce7e5185e8e3d82cbf5b2158887bfae6453bb20
konradudohannes <konrad.woelms@gmail.com>
2020-05-21 20:14:54 +0200

    added a VERSION file to have DRY version numbering

--- datenguidepy/__init__.py
+++ datenguidepy/__init__.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+import os.path
 
 """Top-level package for Datenguide Python."""
 
+version_fp = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../VERSION")
+with open(version_fp) as version_file:
+    version = version_file.readline()
+
 __author__ = """CorrelAid"""
 __email__ = "packages@correlaid.org"
-__version__ = "0.1.1"
+__version__ = version
 
 from datenguidepy.query_builder import Query  # noqa: F401
 from datenguidepy.query_builder import Field  # noqa: F401
